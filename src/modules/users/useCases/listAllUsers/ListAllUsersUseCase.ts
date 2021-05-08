5fdf6dfefa352ff849c11cf21e0ec738ed10e81e
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-05-08 20:02:22 -0300

    Add listAllUsers functionality

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userThatMakesTheRequest = this.usersRepository.findById(user_id);
+
+    if (!userThatMakesTheRequest) {
+      throw new Error(
+        "Error: Hmmm, strange... The user that is making the request does not exist..."
+      );
+    } else if (!userThatMakesTheRequest.admin) {
+      throw new Error(
+        "Error: The user is not an admin... The list cannot be shown"
+      );
+    } else {
+      return this.usersRepository.list();
+    }
   }
 }
 
