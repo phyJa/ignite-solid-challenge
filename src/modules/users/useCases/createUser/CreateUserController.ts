da9f300aea09c128a72b1675788cb593e157db9a
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-05-08 19:09:10 -0300

    Repository functions ready and structure to create a new user (almost) done

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -2,11 +2,23 @@ import { Response, Request } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
+interface IUserRequest {
+  name: string;
+  email: string;
+}
+
 class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email }: IUserRequest = request.body;
+
+    this.createUserUseCase.execute({
+      name,
+      email,
+    });
+
+    return response.status(201).json({ name, email });
   }
 }
 
